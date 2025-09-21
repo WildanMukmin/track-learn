cbdc5388ef17cc4733cb2dad652fcb621da8b7ed
WildanMukmin <wildanmukmin26@gmail.com>
2025-09-21 14:31:11 +0700

    create migration

---
+++ database/migrations/2025_09_21_071236_create_enrollments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('enrollments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('course_id')->constrained('courses')->onDelete('cascade');
+            $table->foreignId('student_id')->constrained('users')->onDelete('cascade');
+            $table->enum('status', [
+                'enrolled',
+                'in_progress',
+                'material_completed',
+                'quiz_attempted',
+                'passed',
+                'certificate_issued'
+            ])->default('enrolled');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('enrollments');
+    }
+};
